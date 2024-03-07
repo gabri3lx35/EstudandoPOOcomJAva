edec75df35facb2497d4ff3140803e0a8c168f34
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2024-03-07 15:02:54 -0300

    1 pilar

---
+++ src/aulas/Aula3.java
@@ -0,0 +1,19 @@
+package aulas;
+
+import interfaces.ControleRemoto;
+
+public class Aula3 {
+    public static void main(String[] args) {
+        ControleRemoto controle1 = new src.classes.ControleRemoto();
+        controle1.ligar();
+        controle1.pause();
+        controle1.play();
+        controle1.menosVolume();
+
+
+
+
+
+        controle1.abrirMenu();
+    }
+}
